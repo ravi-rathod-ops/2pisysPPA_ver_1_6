907105118e2a51c39f2ca7289a7c7e8d3135c044
Sanket Lakhani <sanket@btwebgroup.com>
2025-07-02 23:34:02 +0530

    2 pages completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,33 +8,41 @@ import { AuthService } from './auth.service';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-
   private deferredPrompt: any;
 
-   constructor(private platform: Platform, private router: Router, private authService: AuthService) {
-    console.log("asddsad");
-    
+  constructor(
+    private platform: Platform,
+    private router: Router,
+    private authService: AuthService
+  ) {
+    console.log('asddsad');
+
     this.authService.initializeApp();
 
     window.addEventListener('beforeinstallprompt', (e) => {
-    e.preventDefault();
-    this.deferredPrompt = e;
-  });
+      e.preventDefault();
+      this.deferredPrompt = e;
+    });
   }
 
   showInstallPrompt() {
-  if (this.deferredPrompt) {
-    this.deferredPrompt.prompt();
-    this.deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted install');
-      }
-      this.deferredPrompt = null;
-    });
+    if (this.deferredPrompt) {
+      this.deferredPrompt.prompt();
+      this.deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted install');
+        }
+        this.deferredPrompt = null;
+      });
+    }
   }
-}
-
+  logout() {
+    localStorage.removeItem('userid');
+    localStorage.removeItem('password');
+    localStorage.removeItem('your-data-key');
+    this.router.navigate(['home']);
+  }
 }
